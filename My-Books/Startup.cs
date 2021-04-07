0c207fe7ec06c6a1953de22f940f203090b4dc83
AlaaEldeen <alaaahmed8383@gmail.com>
2021-04-07 17:25:57 +0400

    Added new File

--- My-Books/Startup.cs
+++ My-Books/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using My_Books.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,9 +30,10 @@ namespace My_Books
         {
 
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My_Books", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "My_Books-Updated", Version = "v2" });
             });
         }
 
@@ -41,7 +44,7 @@ namespace My_Books
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My_Books v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v2/swagger.json", "My_Books-Ui-Updated v1"));
             }
 
             app.UseHttpsRedirection();
